b103d01dda324ce8b2a6d5a13738e95fd4864d6c
Beritens <34006542+Beritens@users.noreply.github.com>
2018-05-31 16:21:05 +0200

    localization
    sad

---
+++ Gravity/Assets/scripts/LocalizationData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class LocalizationData{
+    public LocalizationConversations[] conversations;
+	
+}
+[System.Serializable]
+public class LocalizationConversations
+{
+    public string key;
+    public ConText[] text;
+
+}
+[System.Serializable]
+public class ConText
+{
+    public string name;
+    [TextArea(3,10)]
+    public string theText;
+
+}
---
+++ Gravity/Assets/scripts/LocalizationManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LocalizationManager : MonoBehaviour {
+
+    public static LocalizationManager instance;
+    Dictionary<string, ConText[]> Lconversations;
+    bool ready = false;
+
+	void Start () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	public void LoadLocalizedText(string fileName) {
+        Lconversations = new Dictionary<string, ConText[]>();
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+
+            for(int i = 0; i< loadedData.conversations.Length; i++)
+            {
+                Lconversations.Add(loadedData.conversations[i].key, loadedData.conversations[i].text);
+            }
+            Debug.Log("data loaded, dictionary contains " + Lconversations.Count +" entries");
+        }
+        else
+        {
+            Debug.Log("fail");
+        }
+        ready = true;
+	}
+    public bool isReady()
+    {
+        return ready;
+    }
+    public ConText[] GetConText(string key)
+    {
+        ConText[] result;
+        if (Lconversations.ContainsKey(key))
+        {
+            result = Lconversations[key];
+        }
+        else
+        {
+            result = null;
+        }
+        return result;
+    }
+}
