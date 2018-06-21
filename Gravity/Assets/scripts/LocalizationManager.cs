872f2dee0ba3ec8111f7c12563219218da580637
Beritens <34006542+Beritens@users.noreply.github.com>
2018-06-21 23:12:37 +0200

    gh
    hg

--- Gravity/Assets/scripts/LocalizationManager.cs
+++ Gravity/Assets/scripts/LocalizationManager.cs
@@ -9,6 +9,8 @@ public class LocalizationManager : MonoBehaviour {
     Dictionary<string, ConText[]> Lconversations;
     bool ready = false;
     public string defaultFileName;
+    [TextArea(3,5)]
+    public string WebGlText;
 
 	void Start () {
 		if(instance == null)
@@ -25,25 +27,37 @@ public class LocalizationManager : MonoBehaviour {
 	
 	public void LoadLocalizedText(string fileName) {
         Lconversations = new Dictionary<string, ConText[]>();
-        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
 
+        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+        //#if UNITYWEBGL
+        //Resources.Load(fileName) as TextAsset;
+        string dataAsJson;
+        //#endif
         if (File.Exists(filePath))
         {
-            string dataAsJson = File.ReadAllText(filePath);
-            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
-
-            for(int i = 0; i< loadedData.conversations.Length; i++)
-            {
-                Lconversations.Add(loadedData.conversations[i].key, loadedData.conversations[i].text);
-            }
-            Debug.Log("data loaded, dictionary contains " + Lconversations.Count +" entries");
+            dataAsJson = File.ReadAllText(filePath);
         }
         else
         {
-            Debug.Log("fail");
+            print("hello");
+            dataAsJson = WebGlText;
         }
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+
+            for (int i = 0; i < loadedData.conversations.Length; i++)
+            {
+                Lconversations.Add(loadedData.conversations[i].key, loadedData.conversations[i].text);
+            }
+            Debug.Log("data loaded, dictionary contains " + Lconversations.Count + " entries");
+        //}
+        //else
+        //{
+           // print("fail");
+        //}
         ready = true;
-	}
+
+    }
+
     public bool isReady()
     {
         return ready;
